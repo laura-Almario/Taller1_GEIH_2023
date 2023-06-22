2479225c15b6fd1c8715e5b93974267173e9aa48
laura-Almario <l.almarios@uniandes.edu.co>
2023-06-21 20:27:25 -0500

    Create Taller_1.R

---
+++ Scripts/Taller_1.R
@@ -0,0 +1,18 @@
+## Taller 1 - GEIH ##
+## Equipo: Laura Almario, Daniel Orjuela, Daniela Rojas, Anamaria Rodriguez ##
+
+## 1. Llamado de los paquetes de datos y librerias ##
+library(pacman)
+p_load("tidyverse", "rvest", "writexl", "stargazer", "ggplot2", "reshape2", "dplyr", "datasets", "skimr", "gridExtra", "datapasta")
+library(data.table)
+
+## 2. Sraping de los datos (Chunks 1 a 10)
+url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
+data <- data.frame()
+for (i in 1:10) {
+  url_i <- paste0(url, i, ".html")
+  tablas <- url_i %>%
+    read_html() %>%
+    html_table() %>% .[[1]]
+  data <- rbind.data.frame(data, tablas)
+}
